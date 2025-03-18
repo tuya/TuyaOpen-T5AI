145aa40deaa5c46f995b93813da28c285085d266
zhouss@tuya.com <zhouss@tuya.com>
2025-03-18 18:36:51 +0800

    fix platform error

--- platform_config.cmake
+++ platform_config.cmake
@@ -2,6 +2,7 @@ list_subdirectories(PLATFORM_PUBINC_1 ${PLATFORM_PATH}/tuyaos/tuyaos_adapter)
 
 set(PLATFORM_PUBINC_2 
     ${PLATFORM_PATH}/t5_os/bk_idk/include
+    ${PLATFORM_PATH}/t5_os/build/bk7258_cp1/config
 )
 
 set(PLATFORM_PUBINC 
